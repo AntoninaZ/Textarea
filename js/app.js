50461d199227b9502e9a48ecd248346cbc93c40d
Antonina Zhuk <a.zhuk@astoundcommerce.com>
2017-06-21 17:44:53 +0300

    fixed

--- js/app.js
+++ js/app.js
@@ -1,35 +1,78 @@
-/*var MAX_TEXTAREA_COUNT = 140;
-document.querySelector('#js-message').addEventListener('keyup', count);
-document.querySelector('#js-message').addEventListener( 'keypress', maxLength);
-
-/**
- * TextCounter
- */
 var TextCounter = {
-    maxChar: 140,
-    textArea: null,
-    textAreaTotal: null,
-    textAreaLeft: null,
-    init: function() {
-        this.textArea = document.querySelector('#js-message');
-        this.textAreaTotal = document.querySelector('#js-message-left-total');
-        this.textAreaLeft = document.querySelector('#js-message-left-symbols');
-        this.textArea.addEventListener('keypress', this.maxLength);
-        this.textArea.addEventListener('keyup', this.count);
-    },
-    maxLength: function(evt) {
-		if (this.value.length == TextCounter.maxChar) {
-			evt.preventDefault();
-			this.classList.add('uncorrect');
-		}
+	counter: 0,
+	maxChar: 140,
+	insertMode: false,
+	textArea: null,
+	textAreaTotal: null,
+	textAreaLeft: null,
+
+	init: function() {
+		this.textArea = document.querySelector('#js-message');
+		this.textAreaTotal = document.querySelector('#js-message-left-total');
+		this.textAreaLeft = document.querySelector('#js-message-left-symbols');
+
+		this.events();
+	},
+	events: function() {
+		this.textArea.addEventListener('keypress', this.maxLength);
+		this.textArea.addEventListener('keyup', this.count);
+		this.textArea.addEventListener('keyup', this.insertModeOn);
+	},
+	maxLength: function(evt) {
+		this.maxLength = TextCounter.maxChar;
+	},
+	countTotal: function() {
+		TextCounter.textAreaTotal.innerHTML = TextCounter.counter;
+	},
+	countLeft: function() {
+		TextCounter.textAreaLeft.innerHTML = TextCounter.maxChar - TextCounter.counter;
 	},
 	count: function(evt) {
-		var counter = this.value.length;
-		TextCounter.textAreaTotal.innerHTML = counter;
-		TextCounter.textAreaLeft.textContent = TextCounter.maxChar - counter;
+		TextCounter.counter = this.value.length;
+
+		TextCounter.countTotal();
+		TextCounter.countLeft();
 	},
-	
+	setInserMode: function(evt) {
+		if (evt.key === 'Insert') {
+			TextCounter.insertMode = TextCounter.insertMode ? false :  true;
+		} 
+	},
+	isSpecalKeys: function(evt) {
+		switch (evt.key) {
+			case "ArrowDown":
+			case "ArrowUp":
+			case "ArrowLeft":
+			case "ArrowRight":
+			case "Enter":
+			case "Escape":
+			case "Alt":
+			case "Home":
+			case "Escape":
+			case "Alt":
+			case "Home":
+			case "Insert":
+			case "Control":
+			case "Meta":
+			case "End":
+			case "PageUp":
+			case "PageDown":
+			case "Shift":
+			return true;
+			default: false;
+		}
+	},
+	insertModeOn: function(evt) {	
+		TextCounter.setInserMode(evt);
+
+		if (TextCounter.insertMode) {
+			if (!TextCounter.isSpecalKeys(evt)) {
+				var currentSymbol = this.selectionStart;
+				this.value = this.value.substr(0, currentSymbol) + this.value.substr(currentSymbol + 1);
+				this.selectionEnd = currentSymbol;
+			}
+		}
+	}
 };
 
 TextCounter.init();
-//TextCounter.textArea.addEventListener( 'keypress', TextCounter.maxLength);
