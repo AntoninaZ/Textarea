81c0f9eeae7feb5748353b9c76a509bb8da6771f
Antonina Zhuk <a.zhuk@astoundcommerce.com>
2017-06-22 10:56:12 +0300

    added some changes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 var TextCounter = {
 	counter: 0,
 	maxChar: 140,
-	insertMode: false,
 	textArea: null,
 	textAreaTotal: null,
 	textAreaLeft: null,
@@ -11,15 +10,13 @@ var TextCounter = {
 		this.textAreaTotal = document.querySelector('#js-message-left-total');
 		this.textAreaLeft = document.querySelector('#js-message-left-symbols');
 
+		this.textArea.maxLength = TextCounter.maxChar;
+
 		this.events();
 	},
 	events: function() {
 		this.textArea.addEventListener('keypress', this.maxLength);
 		this.textArea.addEventListener('keyup', this.count);
-		this.textArea.addEventListener('keyup', this.insertModeOn);
-	},
-	maxLength: function(evt) {
-		this.maxLength = TextCounter.maxChar;
 	},
 	countTotal: function() {
 		TextCounter.textAreaTotal.innerHTML = TextCounter.counter;
@@ -32,46 +29,6 @@ var TextCounter = {
 
 		TextCounter.countTotal();
 		TextCounter.countLeft();
-	},
-	setInserMode: function(evt) {
-		if (evt.key === 'Insert') {
-			TextCounter.insertMode = TextCounter.insertMode ? false :  true;
-		} 
-	},
-	isSpecalKeys: function(evt) {
-		switch (evt.key) {
-			case "ArrowDown":
-			case "ArrowUp":
-			case "ArrowLeft":
-			case "ArrowRight":
-			case "Enter":
-			case "Escape":
-			case "Alt":
-			case "Home":
-			case "Escape":
-			case "Alt":
-			case "Home":
-			case "Insert":
-			case "Control":
-			case "Meta":
-			case "End":
-			case "PageUp":
-			case "PageDown":
-			case "Shift":
-			return true;
-			default: false;
-		}
-	},
-	insertModeOn: function(evt) {	
-		TextCounter.setInserMode(evt);
-
-		if (TextCounter.insertMode) {
-			if (!TextCounter.isSpecalKeys(evt)) {
-				var currentSymbol = this.selectionStart;
-				this.value = this.value.substr(0, currentSymbol) + this.value.substr(currentSymbol + 1);
-				this.selectionEnd = currentSymbol;
-			}
-		}
 	}
 };
 
